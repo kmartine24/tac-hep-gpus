7ad3221579bd8b5b91f11be3432a166e4caff088
Kyla Martinez <kmartine@g38n11.hep.wisc.edu>
2025-10-12 17:32:52 -0500

    Completed Swap Vectors Exercise

--- week3/swap_vectors.cu
+++ week3/swap_vectors.cu
@@ -1,48 +1,85 @@
 #include <stdio.h>
-
+#include <iostream>
 
 const int DSIZE = 40960;
 const int block_size = 256;
 const int grid_size = DSIZE/block_size;
 
 
-__global__ void vector_addition(FIXME) {
+__global__ void vector_addition(float* h_A, float* h_B) {
 
     //FIXME:
     // Express the vector index in terms of threads and blocks
-    int idx =  ;
-    // Swap the vector elements - make sure you are not out of range
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
+    // Swap the vector elements - make sure you are not out of range
+    if (idx < DSIZE) {
+        float temp = h_A[idx]; 
+        h_A[idx] = h_B[idx];
+	h_B[idx] = temp;
+    } 
 }
 
 
 int main() {
 
 
-    float *h_A, *h_B, *h_C, *d_A, *d_B, *d_C;
+    float *h_A, *h_B, *d_A, *d_B;
     h_A = new float[DSIZE];
     h_B = new float[DSIZE];
-    h_C = new float[DSIZE];
 
 
     for (int i = 0; i < DSIZE; i++) {
         h_A[i] = rand()/(float)RAND_MAX;
         h_B[i] = rand()/(float)RAND_MAX;
-        h_C[i] = 0;
     }
 
+    std::cout << "******** Before Swap ********" << std::endl;
+    std::cout << "h_A = ";
+    for (int i = 0; i < 10; ++i) {
+        std::cout << h_A[i] << " ";
+    }   
+    std::cout << std::endl;
+    std::cout << "h_B = ";
+    for (int i = 0; i < 10; ++i) {
+        std::cout << h_B[i] << " ";
+    }   
+    std::cout << std::endl;
 
     // Allocate memory for host and device pointers 
+    cudaMalloc(&d_A, DSIZE*sizeof(float));
+    cudaMalloc(&d_B, DSIZE*sizeof(float));
 
     // Copy from host to device
-
+    cudaMemcpy(d_A, h_A, DSIZE*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, DSIZE*sizeof(float), cudaMemcpyHostToDevice);
+    
     // Launch the kernel
+    vector_addition <<< grid_size,block_size >>> (d_A, d_B);
 
     // Copy back to host 
+    cudaMemcpy(h_A, d_A, DSIZE*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_B, d_B, DSIZE*sizeof(float), cudaMemcpyDeviceToHost);
 
     // Print and check some elements to make sure swapping was successfull
+    std::cout << "******** After Swap ********" << std::endl;
+    std::cout << "h_A = ";
+    for (int i = 0; i < 10; ++i) {
+        std::cout << h_A[i] << " ";
+    }   
+    std::cout << std::endl;
+    std::cout << "h_B = ";
+    for (int i = 0; i < 10; ++i) {
+        std::cout << h_B[i] << " ";
+    }   
+    std::cout << std::endl;
 
-    // Free the memory 
+    // Free the memory
+    free(h_A);
+    free(h_B);
 
+    cudaFree(d_A);
+    cudaFree(d_B);
+    
     return 0;
 }
